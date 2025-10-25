620eb48be739429982e609f4639699f1849074b0
ali-heidari <ali-heidari@outlook.com>
2025-10-25 06:07:52 +0330

    Add csv dataset

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ use aion_math::math::Math;
 use color_eyre::Result;
 use crossterm::event::{self, Event};
 use ratatui::{DefaultTerminal, Frame};
+use std::ops::Div;
+use std::path::Path;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::time::sleep;
@@ -29,21 +31,52 @@ fn use_tui() -> Result<(), color_eyre::Report> {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     env_logger::init();
     let mut state = SyntheticState::new();
-    let node = Arc::new(Node::new());
+    let node = Arc::new(Node::new(0.05, 0.5));
 
     let n = node.clone();
-    Worker::start(n);
-    
-    let mut math = Math::new();
-    loop {
-        let features = state.next();
-        node.next(features, &mut math);
+    Node::start_training(n);
 
-        sleep(Duration::from_millis(10)).await;
+    let mut math = Math::new();
+    // println!(
+    //     "(vec![cpu usage,memory usage, swap, disk usage, throughput, latency],ACTION= your answer)"
+    // );
+
+    // loop {
+    //     // *node.batch_sampled.write().unwrap() = true;
+    //     let features = state.next();
+    //     // println!(
+    //     //     "(vec![{:.3}, {:.3}, {:.3}, {:.3}, {:.3}, {:.3}],ACTION)",
+    //     //     features[0], features[1], features[2], features[3], features[4], features[5]
+    //     // );
+
+    //     node.next(features, &mut math);
+
+    //     sleep(Duration::from_millis(10)).await;
+    // }
+    let file_path = Path::new("vmCloud_data.csv");
+    let mut rdr = csv::Reader::from_path(file_path)?;
+
+        use rand::Rng;
+        let mut rng = rand::thread_rng();
+
+    // Iterate over the records
+    for result in rdr.records() {
+        let record = result?;
+        if record.get(2).unwrap().is_empty() || record.get(3).unwrap().is_empty() {
+            continue;
+        }
+        let max=1.0;//if rng.gen_bool(0.04) {1.0} else {0.4};
+        let features = state.next(
+            record.get(2).unwrap().parse::<f32>().unwrap().div(100.0).clamp(0.05, max),
+            record.get(3).unwrap().parse::<f32>().unwrap().div(100.0).clamp(0.05, max),
+        );
+        node.next(features, &mut math);
+         sleep(Duration::from_millis(10)).await;
     }
+    Ok(())
 }
 
 fn run(mut terminal: DefaultTerminal) -> Result<()> {

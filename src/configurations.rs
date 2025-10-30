adde3b338d847cac8f3da181d9bf559f87ea4c36
ali-heidari <ali-heidari@outlook.com>
2025-10-30 10:09:46 +0330

    Add mode and interval_secs to configurations

--- src/configurations.rs
+++ src/configurations.rs
@@ -1,8 +1,10 @@
 use serde::Deserialize;
 
+use crate::node::RunningMode;
+
 #[derive(Deserialize, Debug, Clone)]
 pub struct Configurations {
-    pub train_interval_secs: u64,
+    pub interval_secs: u64,
     pub batch_size: u32,
     pub total_batches: usize,
     pub input_number: usize,
@@ -10,5 +12,6 @@ pub struct Configurations {
     pub hidden_layers: usize,
     pub reply_capacity: usize,
     pub model_name: String,
-    pub log_interval: u64
+    pub log_interval: u64,
+    pub mode: RunningMode,
 }
